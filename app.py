8fb3df4837c25defc25db8a1dd4f4ae9c3916b26
Theresah Essuman <hasereth6@gmail.com>
2024-08-14 12:47:58 +0000

    modeling

--- app.py
+++ app.py
@@ -2,15 +2,11 @@ import streamlit as st
 
 st.set_page_config(
     page_title="ML Prediction App",
-    page_icon=":bar_chart:",
+    page_icon="🏠",
     layout="wide"
 )
 
 st.sidebar.success("Select a page from the sidebar above")
 st.title("Embedded ML Model Analysis App")
-st.sidebar.title("Home")
-st.sidebar.markdown("View Data")
-st.sidebar.markdown("Dashboard")
-st.sidebar.markdown("Pedict")
-st.sidebar.markdown("History")
+
 

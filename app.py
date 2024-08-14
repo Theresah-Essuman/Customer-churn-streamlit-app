ca31e35587ddffe1dd37b76209b78ef36d27af1c
Theresah Essuman <hasereth6@gmail.com>
2024-08-14 12:05:56 +0000

    Page setup

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st 
 
 st.set_page_config(
-    page_title="ML Prediction Dashboard ",
+    page_title="ML Prediction App",
     page_icon=":bar_chart:",
-    )
+    layout="wide"
+)
+
+st.sidebar.success("Select a page from the sidebar above")
+st.title("Embedded ML Model Analysis App")
+st.sidebar.title("Home")
+st.sidebar.markdown("View Data")
+st.sidebar.markdown("Dashboard")
+st.sidebar.markdown("Pedict")
+st.sidebar.markdown("History")
+
